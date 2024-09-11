7867e5e235f8827e893a6434ce4fa46fa019a59a
RajeshCodingBlck <137406636+RajeshCodingBlck@users.noreply.github.com>
2024-09-11 15:03:27 +0530

    Create CaseStudy2.java

---
+++ inheritance/CaseStudy2.java
@@ -0,0 +1,31 @@
+// "static void main" must be defined in a public class.
+// Case Study-1
+class A{
+    
+    int d=12;
+    A(int d){
+        this.d=d;
+    }
+}
+class B extends A{
+    
+    int a=12;
+    B(int d, int a){
+       
+         this.a=a;
+         super(d);
+    }
+    
+}
+public class Main {
+    public static void main(String[] args) {
+        
+        B obj= new B(30, 40);
+        System.out.println(obj.d); 
+
+      
+        // Output
+        // Error 
+        // Super call from child constructor must be a First Statement .
+    }
+}
